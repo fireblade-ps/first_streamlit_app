07253ab576ffd6bf61599fff42b0eeb77d34df4c
fireblade-ps <135445728+fireblade-ps@users.noreply.github.com>
2023-06-03 21:16:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinatch & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Eggs')
