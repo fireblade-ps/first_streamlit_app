b7cee59c6b33288490e5f5d4fb6fba1d742e84c2
fireblade-ps <135445728+fireblade-ps@users.noreply.github.com>
2023-06-03 22:07:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build your own Smoothie 🥝🍇')
 # Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.dataframe(my_fruit_list)
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit), ['Avocado','Strawberries'])
 streamlit.text(fruits_selected)
 fruits_to_show = my_fruit_list.loc[fruits_selected]
