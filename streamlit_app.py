3801f337b426bdfa259ee9fbe3661af72892c943
fireblade-ps <135445728+fireblade-ps@users.noreply.github.com>
2023-06-04 14:16:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import requests
 import snowflake.connector
  
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt").set_index('Fruit')
-
+ 
 #app_code
 streamlit.title('My Parents healthy diner')
 
